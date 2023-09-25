8361596efe4d246e8ea24f7b9e1efb48aef04da3
isaacainamaani <aina.isaac2002@gmail.com>
2023-09-25 05:43:51 +0300

    Material UI working

---
+++ src/pages/AddBook.tsx
@@ -0,0 +1,14 @@
+import { Typography } from "@material-ui/core";
+
+const AddBook = ():JSX.Element => {
+    return ( 
+            <div>
+                <h2>Adding.....</h2>
+                <Typography variant="h4" >
+                    Add book
+                </Typography>
+            </div>  
+     );
+}
+ 
+export default AddBook;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+const Homepage = ():JSX.Element => {
+    return ( 
+        <h2>LibraryManagementSystemFrontend</h2>
+     );
+}
+ 
+export default Homepage;
